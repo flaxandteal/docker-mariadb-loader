6cd537751165350098c56ef26f4a3a1da5d362e9
Phil Weir <phil.weir@numa.ie>
2015-11-14 11:46:09 +0000

    env configurable unprivileged user

--- config/mariadb-start.sh
+++ config/mariadb-start.sh
@@ -4,7 +4,16 @@
 directory="/data/mariadb/data"
 if [ ! "$(ls -A $directory)" ]; then
     /usr/bin/mysql_install_db --datadir=/data/mariadb/data --user=mysql
-    /usr/bin/mysqld_safe --init-file=/opt/bin/mariadb-setup.sql
+    cp /opt/bin/mariadb-setup.sql /tmp/combined.sql
+    cat >> /tmp/combined.sql <<-EOSQL
+	CREATE USER '${LARAVEL_DB_USER}'@'%' IDENTIFIED BY PASSWORD('${LARAVEL_DB_PASS}');
+	CREATE DATABASE '${LARAVEL_DB_NAME}';
+	GRANT ALL PRIVILEGES ON \`${LARAVEL_DB_NAME}\`.* To '${LARAVEL_DB_USER}'@'%' IDENTIFIED BY '${LARAVEL_DB_PASS}';
+	FLUSH PRIVILEGES;
+	CONNECT '${LARAVEL_DB_NAME}';
+EOSQL
+    cat /data/testdata.sql >> /tmp/combined.sql
+    /usr/bin/mysqld_safe --init-file=/tmp/combined.sql
 else
     /usr/bin/mysqld_safe
 fi
